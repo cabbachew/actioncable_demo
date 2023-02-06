68a34ebd0ff71952234230bba3108dafe1ee804b
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-06 12:42:35 -0800

    seed db with a couple of sample users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: 'example@email.com', password: 'password')
+User.create(email: 'jarkmanzer@gmail.com', password: 'password')
