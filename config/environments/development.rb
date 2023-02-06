980a508d34f491cdb563a5a0b50c48081d86b22f
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-05 17:33:03 -0800

    define default url options for development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Set up default URL options for Devise mailer in development
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
