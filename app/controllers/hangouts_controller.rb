7510da91d09aea3295df9472cc98efe6405aab2a
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-19 14:25:26 -0800

    modify index action and view to display messages

--- app/controllers/hangouts_controller.rb
+++ app/controllers/hangouts_controller.rb
@@ -1,5 +1,8 @@
 class HangoutsController < ApplicationController
   def index
     @message = Message.new
+    # Circumvent N+1 query problem
+      # .all is not necessary because .each is called on @messages in the view
+    @messages = Message.includes(:user)
   end
 end
