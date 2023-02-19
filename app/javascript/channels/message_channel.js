a771610ae245e300c4bb3821ad075ad0c064d832
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-19 15:23:06 -0800

    modify client behavior to render real-time messages and clear input

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -13,6 +13,11 @@ const messageChannel = consumer.subscriptions.create("MessageChannel", {
     // Called when there's incoming data on the websocket for this channel
     const messageDisplay = document.querySelector('#message-display')
     messageDisplay.insertAdjacentHTML('beforeend', this.template(data))
+
+    // Scroll to the last message
+    messageDisplay.scrollTop = messageDisplay.scrollHeight;
+    // Clear the input
+    document.querySelector('#message-input').value = ''
   },
 
   template(data) {
@@ -21,24 +26,35 @@ const messageChannel = consumer.subscriptions.create("MessageChannel", {
                 <p>${data.user.email}</p>
               </div>
               <div class="message-body">
-                <p>${data.message.body}</p>
+                <p>${data.body}</p>
               </div>
             </article>`
   }
 });
 
-document.addEventListener("turbo:load", () => {
-  let form = document.querySelector('#message-form')
-  if(form) {
-    form.addEventListener('submit', (e) => {
-      e.preventDefault()
-      let messageInput = document.querySelector('#message-input').value
-      if(messageInput == '') return;
-      const message = {
-        body: messageInput
-      }
-      messageChannel.send({message: message})
-      document.querySelector('#message-input').value = '' // Clear the input
-    })
-  }
-})
+  // Scroll to the last message when the page loads
+  window.addEventListener("load", function() {
+    var messageDisplay = document.getElementById("message-display");
+    // scrollTop means the distance from the top of the element to the top of the visible area
+    // scrollHeight means the height of the element's content, including content not visible on the screen due to overflow
+    // So, when the page loads, we set the scrollTop to the scrollHeight, which means we scroll to the bottom of the element
+    messageDisplay.scrollTop = messageDisplay.scrollHeight;
+  });
+
+// Directly send user input to the channel
+
+// document.addEventListener("turbo:load", () => {
+//   let form = document.querySelector('#message-form')
+//   if(form) {
+//     form.addEventListener('submit', (e) => {
+//       e.preventDefault()
+//       let messageInput = document.querySelector('#message-input').value
+//       if(messageInput == '') return;
+//       const message = {
+//         body: messageInput
+//       }
+//       messageChannel.send({message: message})
+//       document.querySelector('#message-input').value = '' // Clear the input
+//     })
+//   }
+// })
